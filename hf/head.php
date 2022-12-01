7f95840393f05211bde710b5239cffd2fd6f27d0
Jesse <you@example.com>
2022-11-30 19:49:35 -0600

    Changes to be committed: 	modified:   hf/head.php

--- hf/head.php
+++ hf/head.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>FACTOR TECNOLOGIA</title>
+  <title>La Ferreteria</title>
   <link rel="stylesheet" href="/Tienda/archivos/css/normalize.css" />
   <link href="https://fonts.googleapis.com/css2?family=Play&display=swap" rel="stylesheet" />
   <script src="https://kit.fontawesome.com/2803c554a7.js" crossorigin="anonymous"></script>
